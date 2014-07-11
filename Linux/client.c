cd0363af68cbcd730824fd966201bdb1876e7c85
smallcattom <397579875yl@gmail.com>
2014-07-11 09:40:32 +0800

    start

---
+++ Linux/client.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+int main()
+{
+
+    return 0;
+}
+

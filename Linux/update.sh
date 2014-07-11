a48e3dcfefe4361cd22a0b5356ae5e2f16989ca9
smallcattom <397579875yl@gmail.com>
2014-07-11 22:39:52 +0800

    script program for git push

--- Linux/update.sh
+++ Linux/update.sh
@@ -3,6 +3,17 @@ if [ $# -eq 0 ]
 then
 exit 1
 fi
-git add $1
-git commit -m $2
+
+a=`ls | grep $1`
+if [ $a != $1 ]
+then
+echo "file $1 is not exist.\nPlease try again."
+exit 1
+fi
+name=$1
+shift
+desc=$@
+#echo $desc,$name
+git add $name
+git commit -m "$desc"
 git push origin master

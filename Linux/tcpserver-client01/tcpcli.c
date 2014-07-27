faed3ec700ff0651fead6cf3c461a7255cdc7d35
smallcattom <397579875yl@gmail.com>
2014-07-28 01:51:28 +0800

    tcpserv.c myTcp

--- Linux/tcpserver-client01/tcpcli.c
+++ Linux/tcpserver-client01/tcpcli.c
@@ -15,12 +15,12 @@
 void str_cli(FILE *fp, int sockfd)
 {
 	char sendline[MAXLINE], recvline[MAXLINE];
-	while (Fgets(sendline, MAXLINE, fp) != NULL){
-		Writen(sockfd, sendline, strlen(sendline));
-		if (Readline(sockfd, recvline, MAXLINE) == 0)
-			err_quit("str_cli: server terminated prematurely");
+	while (fgets(sendline, MAXLINE, fp) != NULL){
+		writen(sockfd, sendline, strlen(sendline));
+		if (readline(sockfd, recvline, MAXLINE) == 0)
+			perror("str_cli: server terminated prematurely");
 
-		Fputs(recvline, stdout);
+		fputs(recvline, stdout);
 	}
 }
 int main(int argc,char **argv)
@@ -29,12 +29,12 @@ int main(int argc,char **argv)
 	struct sockaddr_in servaddr;
 	if (argc != 2)
 		err_quit("usage: tcpcli <IPaddress>");
-	sockfd = Socket(AF_INET,SOCK_STREAM, 0);
+	sockfd = socket(AF_INET,SOCK_STREAM, 0);
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(9877);
-	Inet_pton(AF_INET,argv[1], &servaddr.sin_addr);
-	Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+	inet_pton(AF_INET,argv[1], &servaddr.sin_addr);
+	connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
 	str_cli(stdin, sockfd);
 	exit(0);
     return 0;

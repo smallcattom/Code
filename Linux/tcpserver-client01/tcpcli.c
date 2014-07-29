ad0618c30540c6ea40c809d116f0a12d1bd30f25
smallcattom <397579875yl@gmail.com>
2014-07-29 17:01:27 +0800

    tcpserv.c

--- Linux/tcpserver-client01/tcpcli.c
+++ Linux/tcpserver-client01/tcpcli.c
@@ -16,8 +16,8 @@ void str_cli(FILE *fp, int sockfd)
 {
 	char sendline[MAXLINE], recvline[MAXLINE];
 	while (fgets(sendline, MAXLINE, fp) != NULL){
-		writen(sockfd, sendline, strlen(sendline));
-		if (readline(sockfd, recvline, MAXLINE) == 0)
+		write(sockfd, sendline, strlen(sendline));
+		if (read(sockfd, recvline, MAXLINE) == 0)
 			perror("str_cli: server terminated prematurely");
 
 		fputs(recvline, stdout);
@@ -28,7 +28,7 @@ int main(int argc,char **argv)
 	int sockfd;
 	struct sockaddr_in servaddr;
 	if (argc != 2)
-		err_quit("usage: tcpcli <IPaddress>");
+		perror("usage: tcpcli <IPaddress>");
 	sockfd = socket(AF_INET,SOCK_STREAM, 0);
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;

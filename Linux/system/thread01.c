3d58375a2273b014b0c86616abd43bbd628e34c1
smallcattom <397579875yl@gmail.com>
2014-08-04 00:21:45 +0800

    start system programming

---
+++ Linux/system/thread01.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <fcntl.h>
+#include <math.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+pthread_t ntid;
+void printids(const char*s)
+{
+	pid_t pid;
+	pthread_t tid;
+	pid = getpid();
+	tid = pthread_self();
+	printf("%s pid %u tid %u (0x%x)\n", s, (unsigned int)pid,
+			(unsigned int)tid,(unsigned int)tid);
+}
+void * thr_fn(void *arg)
+{
+	printids("new thread: ");
+	return ((void *)0);
+	return NULL;
+}
+int main()
+{
+	int err;
+	err = pthread_create(&ntid, NULL,thr_fn, NULL);
+	if (err != 0)
+		printf("can't create thread: %s\n", strerror(err));
+	printids("main thread:");
+	sleep(3);
+	exit(0);
+    return 0;
+}

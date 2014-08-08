b1d9e4f2a46adbd2d8362145c778f75541ae5bf2
smallcattom <397579875yl@gmail.com>
2014-08-08 10:55:49 +0800

    modify

--- Linux/system/thread_join.c
+++ Linux/system/thread_join.c
@@ -10,57 +10,33 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <pthread.h>
-void cleanup(void *arg)
-{
-	printf("cleanup: %s\n", (char*)arg);
-}
-void * thr_fn1(void *arg)
-{
-	printf("thread 1 start\n");
-	pthread_cleanup_push(cleanup, "thread 1 first handler");
-	pthread_cleanup_push(cleanup,"thread 1 second handler");
-	printf("thread 1 push complete\n");
-	if (arg)
-		return ((void*)1);
-	pthread_cleanup_pop(0);
-	pthread_cleanup_pop(0);
+void *thr_fn1(void *arg){
+	printf("thread 1 returning\n");
 	return ((void*)1);
 }
-void * thr_fn2(void *arg)
-{
-	printf("thread 2 start\n");
-	pthread_cleanup_push(cleanup, "thread 2 first handler");
-	pthread_cleanup_push(cleanup, "thread 2 second handler");
-	printf("thread 2 push complete\n");
-	if (arg)
-		pthread_exit((void *)2);
-	pthread_cleanup_pop(0);
-	pthread_cleanup_pop(0);
+void *thr_fn2(void *arg){
+	printf("thread 2 exiting\n");
 	pthread_exit((void*)2);
 }
-/*
- *如果线程是通过从它的启动例程中返回而终止的话，那么它的清理处理程序就不会被调用，清理处理程序是按照安装时相反的顺序调用的。
- * */
 int main()
 {
-	int err;
-	pthread_t tid1, tid2;
-	void *tret;
-	
-	err = pthread_create(&tid1, NULL, thr_fn1, (void *)1);
-	if (err != 0)
-		printf("can't create thread 1: %s\n", strerror(err));
-	err = pthread_create(&tid2, NULL, thr_fn2, (void *)1);
-	if (err != 0)
-		printf("can't create thread 2: %s\n", strerror(err));
-	err = pthread_join(tid1, &tret);
-	if (err != 0)
-		printf("can't join with thread 1: %s\n",strerror(err));
-	printf("thread 1 exit code %d\n",(int)tret);
-	err = pthread_join(tid2, &tret);
-	if (err != 0)
-		printf("can't join with thread 2: %s\n",strerror(err));
-	printf("thread 2 exit code %d\n",(int)tret);
-	exit(0);
+	   int err;
+	   pthread_t tid1, tid2;
+	   void *tret = NULL;
+	   err = pthread_create(&tid1, NULL, thr_fn1, NULL);
+	   if(err != 0)
+		   printf("can't create thread 1: %s\n",strerror(err));
+	   err = pthread_create(&tid2, NULL, thr_fn2, NULL);
+	   if (err != 0)
+		   printf("cant't create thread 2: %s\n",strerror(err));
+
+	   err = pthread_join(tid1, &tret);
+	   if (err != 0)
+		   printf("can't join with thread 1: %s\n", strerror(err));
+	   printf("thread 1 exit code %d\n",(int)tret);
+	   err = pthread_join(tid2, &tret);
+	   if (err != 0)
+		   printf("can't join with thread 2: %s\n", strerror(err));
+	   printf("thread 2 exit code %d\n", (int)tret);
+	   exit(0);
 }

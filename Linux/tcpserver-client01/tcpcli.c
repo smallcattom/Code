f3c641dc3055b2e87e873984983a8ec358834d60
smallcattom <397579875yl@gmail.com>
2014-08-20 18:54:23 +0800

    read return -1 refer to error

--- Linux/tcpserver-client01/tcpcli.c
+++ Linux/tcpserver-client01/tcpcli.c
@@ -17,7 +17,7 @@ void str_cli(FILE *fp, int sockfd)
 	char sendline[MAXLINE], recvline[MAXLINE];
 	while (fgets(sendline, MAXLINE, fp) != NULL){
 		write(sockfd, sendline, strlen(sendline));
-		if (read(sockfd, recvline, MAXLINE) == 0)
+		if (read(sockfd, recvline, MAXLINE) < 0)
 			perror("str_cli: server terminated prematurely");
 
 		fputs(recvline, stdout);
